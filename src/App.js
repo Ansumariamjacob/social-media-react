adbbaf8a3abc8cc3145ed44a53921c0743dbf90f
ansumariamjacob <ansumariam99@gmail.com>
2022-05-18 13:16:01 +0530

    viewCompleted

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Addpost from './component/Addpost';
+import Searchpost from './component/Searchpost';
+import Viewpost from './component/Viewpost';
 
 function App() {
   return (
     <div >
      <Addpost/>
+     <Searchpost/>
+     <Viewpost/>
+
     </div>
   );
 }
